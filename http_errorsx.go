3c0d739e9f0fc7b70c3b1cb32c569749c953047f
James <jr39.dev@gmail.com>
2024-07-15 23:54:42 +0200

    use slog for HTTP logger

--- http_errorsx.go
+++ http_errorsx.go
@@ -2,21 +2,17 @@ package errorsx
 
 import (
 	"encoding/json"
+	"log/slog"
 	"net/http"
 )
 
-type logger interface {
-	Warn(message string, args ...interface{})
-	Error(message string, args ...interface{})
-}
-
-// HTTPError writes a warning or error to the logger and writes the error message as plain text to the ResponseWriter
-func HTTPError(w http.ResponseWriter, log logger, err Error, statusCode int) {
+// HTTPError writes a warning or error to the slog logger and writes the error message as plain text to the ResponseWriter
+func HTTPError(w http.ResponseWriter, err Error, statusCode int) {
 	w.WriteHeader(statusCode)
 	if statusCode < 500 {
-		log.Warn("%s. Stack trace:\n%s", err.Error(), err.Stack())
+		slog.Warn("%s. Stack trace:\n%s", err.Error(), err.Stack())
 	} else {
-		log.Error("%s. Stack trace:\n%s", err.Error(), err.Stack())
+		slog.Error("%s. Stack trace:\n%s", err.Error(), err.Stack())
 	}
 
 	w.Write([]byte(err.Error()))
@@ -26,13 +22,13 @@ type JSONErrorMessageType struct {
 	Message string `json:"message"`
 }
 
-// HTTPJSONError writes a warning or error to the logger and writes the error message as the Message property a JSONErrorMessageType
-func HTTPJSONError(w http.ResponseWriter, log logger, err Error, statusCode int) {
+// HTTPJSONError writes a warning or error to the slog logger and writes the error message as the Message property a JSONErrorMessageType
+func HTTPJSONError(w http.ResponseWriter, err Error, statusCode int) {
 	w.WriteHeader(statusCode)
 	if statusCode < 500 {
-		log.Warn("%s. Stack trace:\n%s", err.Error(), err.Stack())
+		slog.Warn(ErrWithStack(err).Error())
 	} else {
-		log.Error("%s. Stack trace:\n%s", err.Error(), err.Stack())
+		slog.Error(ErrWithStack(err).Error())
 	}
 
 	json.NewEncoder(w).Encode(JSONErrorMessageType{err.Error()})
